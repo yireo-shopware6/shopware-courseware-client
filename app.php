c832dac71ae12bd5280826c35a64d1c78687a7b8
Rico Neitzel <rico@buro71a.de>
2021-07-13 09:58:33 +0200

    adds $reader TypeHint

--- app.php
+++ app.php
@@ -1,8 +1,11 @@
 <?php declare(strict_types=1);
 
 use League\Plates\Engine as PlatesEngine;
+use Shopware\Courseware\Filesystem\Reader;
 
+/** @var Reader $reader */
 require_once 'bootstrap.php';
+
 
 $templateEngine = new PlatesEngine(__DIR__ . '/templates');
 $page = $_GET['page'] ?? 'course-overview';
